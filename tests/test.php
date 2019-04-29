2819e19196b9e1d590b6a35d19971bb5ed9c8104
himanshudwivedi <h.dwivedi@accenture.com>
2019-04-29 12:15:20 +0530

    added file

---
+++ tests/test.php
@@ -0,0 +1,19 @@
+<?php ini_set('max_execution_time', 3000); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Test file</title>
+<style>
+
+</style>
+</head>
+
+<body>
+<h1>Test</h1>
+
+<?php 
+echo 'This is PHP Code';
+?>
+</body>
+</html>
